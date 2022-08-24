dddbf9c0c91ddb19b505d5ca69f0023a814bc037
hector varela <hector.varela@thoughtworks.com>
2022-08-24 19:52:15 -0400

    test
    Signed-off-by: hector varela <hector.varela@thoughtworks.com>

--- devops/jenkins/Jenkinsfile-delivery-dev-pipeline.groovy
+++ devops/jenkins/Jenkinsfile-delivery-dev-pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo  env.BRANCH_NAME
+                echo 'Pulling...' + env.BRANCH_NAME
                 echo 'Hello World Yape!!!!'
             }
         }
