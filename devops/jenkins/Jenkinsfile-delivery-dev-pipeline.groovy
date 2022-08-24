c0752817c913accd6caec046a392be4bec3b67ea
hector varela <hector.varela@thoughtworks.com>
2022-08-24 13:25:52 -0400

    agrega   ejemplo stage
    Signed-off-by: hector varela <hector.varela@thoughtworks.com>

--- devops/jenkins/Jenkinsfile-delivery-dev-pipeline.groovy
+++ devops/jenkins/Jenkinsfile-delivery-dev-pipeline.groovy
@@ -1,2 +1,11 @@
 @Library('jenkins-sharedlib@feature/databricks-refactor')
 import sharedlib.DatabricksJenkinsUtil
+
+agent any
+stages {
+    stage('Hello') {
+        steps {
+            echo 'Hello World'
+        }
+    }
+}
